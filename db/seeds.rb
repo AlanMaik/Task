c7d14d475caf195db59b41c7523b9a2c5483aebd
AlanMaik <alanmaikm@gmail.com>
2019-06-29 23:49:49 -0300

    Add seeds

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+Task.create(title: 'Learning Ruby on Rails', done: true)
+Task.create(title: 'Learning React', done: true)
+Task.create(title: 'Create API with ROR', done: true)
+Task.create(title: 'Create Web with React', done: true)
